b1714872dc511a23f5ce24278effe51764ff7ea0
khong trung son <khongtrungson2004@gmail.com>
2025-03-14 04:41:48 +0700

    Change the data type of the time field in the Job class to ZonedDateTime, but store it as a string.

--- src/main/java/com/example/todolist/models/Job.java
+++ src/main/java/com/example/todolist/models/Job.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 
 import java.time.LocalDateTime;
 import java.time.ZonedDateTime;
